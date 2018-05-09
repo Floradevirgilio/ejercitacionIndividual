51cb28d1f3f438884e51e4faa84b121e03b1532f
Floradevirgilio <fdevirgilio@hotmail.com>
2018-05-09 17:25:37 -0300

    generado datos php, punto 1

---
+++ datos.php
@@ -0,0 +1,18 @@
+<?php
+$datos = [
+	   "id" => 3187592,
+	   "nickname"=> "VENDEDOR-ABC",
+	   "registration_date" => "2010-08-07",
+	   "country_id" => "AR",
+	   "address" => ["city" =>"Belgrano",
+		               "state" => "CABA"],
+	   "user_type" => "normal",
+   	 "reputation" => 10156,
+     "country" => "ARG",
+  	 "points" => ["level": "5",
+		              "transactions" => ["canceled" => 8372,
+			                               "completed" => 59226,
+			                               "total" => 67598]
+	               ],
+	   "status" => ["site_status" => "active"]
+];
